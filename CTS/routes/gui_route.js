f01e9a82ce9b722c720fd4622f6da61abce40ab3
Dingensen <f_nieb02@uni-muenster.de>
2021-07-29 11:50:31 +0200

    initialise room overview page.
    very reductive list view with according sensors. No live feed yet.

--- CTS/routes/gui_route.js
+++ CTS/routes/gui_route.js
@@ -9,13 +9,27 @@ const Aoi = require('./../models/aoi');
 /**
  * this get-endpoint supplies the webpage overview of the CTS
  */
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
     //TODO: this will be an Object that contains a representation of all the rooms we can view
-    const building = null;
+    const building = await Room.find();
 
-    res.render('index');
+    for(room of building){
+        await populatePss(room);
+        await populateRSS(room);
+    }
+
+    res.render('index', {rooms: building});
 })
 
+async function populatePss(room){
+    for(pss in room.passages){
+        room.passages[pss] = await Passage.findById(room.passages[pss]._id);
+    }
+};
+
+async function populateRSS(room){
+    room.aoi = await Aoi.findById(room.aoi);
+};
 
     
 module.exports = router;
